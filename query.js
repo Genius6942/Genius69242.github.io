ebb85b452628d521800e4001624805079046db82
a person <74061612+Genius6942@users.noreply.github.com>
2021-11-14 07:56:24 -0500

    Update query.js

--- query.js
+++ query.js
@@ -2,3 +2,4 @@ window.$ = window.query = function (query) {
 	const r = document.querySelectorAll(query);
 	return r.length > 1 ? r : r.length === 1 ? r[0] : null;
 }
+HTMLElement.prototype.on = HTMLElement.prototype.addEventListener;
