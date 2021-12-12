db90a599bfa08c668aa1fc45a8124704f1e974af
a person <74061612+Genius6942@users.noreply.github.com>
2021-12-12 16:09:13 -0500

    Update query.js

--- query.js
+++ query.js
@@ -2,4 +2,8 @@ window.$ = window.query = function (query) {
 	const r = document.querySelectorAll(query);
 	return r.length > 1 ? r : r.length === 1 ? r[0] : null;
 }
-HTMLElement.prototype.on = HTMLElement.prototype.addEventListener;
+HTMLElement.prototype.on = function (a,b,c) {
+	for (let x of a.split(' ')) {
+		this.addEventListener(x, b, c);
+	}
+}
